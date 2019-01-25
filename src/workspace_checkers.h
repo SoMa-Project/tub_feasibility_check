4722c09bbc25d4fc451dd125d2a6394e4278ab7a
Ilia Krets <krets@campus.tu-berlin.de>
2019-01-25 17:37:20 +0100

    Separate workspace position and orientation check

--- src/workspace_checkers.h
+++ src/workspace_checkers.h
@@ -1,6 +1,8 @@
 #ifndef WORKSPACE_CHECKERS_H
 #define WORKSPACE_CHECKERS_H
 
+#include <rl/math/Vector.h>
+#include <rl/math/Rotation.h>
 #include <rl/math/Transform.h>
 #include <boost/array.hpp>
 
@@ -8,41 +10,49 @@
 class WorkspaceChecker
 {
 public:
-  virtual ~WorkspaceChecker();
-  virtual bool contains(const rl::math::Transform& transform) const = 0;
+  typedef std::function<bool(const rl::math::Rotation&)> CheckOrientation;
+  typedef std::function<bool(const rl::math::Vector3&)> CheckPosition;
+
+  WorkspaceChecker(CheckPosition position_check, CheckOrientation orientation_check);
+
+  bool contains(const rl::math::Transform& transform_to_check);
+
+private:
+  CheckOrientation orientation_check_;
+  CheckPosition position_check_;
 };
 
 /* A manifold with positions inside of a box and orientations having a maximum value of Euler XYZ rotation.
  * The allowed Euler angles are the following: X (-pi, pi), Y (-pi/2, pi/2), Z (-pi, pi). contains will not
  * function properly when the angles are outside of the allowed ranges.
  */
-class BoxChecker : public WorkspaceChecker
+class BoxPositionChecker
 {
 public:
-  BoxChecker(const rl::math::Transform& center_pose, boost::array<double, 3> min_position_deltas,
-             boost::array<double, 3> max_position_deltas, boost::array<double, 3> min_XYZ_orientation_deltas,
-             boost::array<double, 3> max_XYZ_orientation_deltas)
-    : center_pose_(center_pose)
-    , min_position_deltas_(min_position_deltas)
-    , max_position_deltas_(max_position_deltas)
-    , min_XYZ_orientation_deltas_(min_XYZ_orientation_deltas)
-    , max_XYZ_orientation_deltas_(max_XYZ_orientation_deltas)
-  {
-  }
-
-  ~BoxChecker() override;
-
-  bool contains(const rl::math::Transform& transform) const override;
+  BoxPositionChecker(const rl::math::Transform& center_pose, boost::array<double, 3> min_position_deltas,
+                     boost::array<double, 3> max_position_deltas, double position_epsilon = 1e-5);
+  bool operator()(const rl::math::Vector3& position_to_check);
 
 private:
   rl::math::Transform center_pose_;
   boost::array<double, 3> min_position_deltas_;
   boost::array<double, 3> max_position_deltas_;
+  double position_epsilon_;
+};
+
+class AroundTargetOrientationChecker
+{
+public:
+  AroundTargetOrientationChecker(rl::math::Rotation target_orientation,
+                                 boost::array<double, 3> min_XYZ_orientation_deltas,
+                                 boost::array<double, 3> max_XYZ_orientation_deltas, double angle_epsilon = 1e-2);
+  bool operator()(const rl::math::Rotation& orientation_to_check);
+
+private:
+  rl::math::Rotation target_orientation_;
   boost::array<double, 3> min_XYZ_orientation_deltas_;
   boost::array<double, 3> max_XYZ_orientation_deltas_;
-
-  /* absolute precision for comparing euler angles */
-  const double angle_epsilon_ = 1e-2;
+  double angle_epsilon_;
 };
 
 #endif  // WORKSPACE_CHECKERS_H
--- test/test_workspace_checker_and_sampler.cpp
+++ test/test_workspace_checker_and_sampler.cpp
@@ -1,18 +1,26 @@
-#define BOOST_TEST_MODULE test workspace checker and sampler
-
-#include <boost/test/included/unit_test.hpp>
+#include <gtest/gtest.h>
 #include <random>
 #include <rl/math/Transform.h>
 #include <rl/math/Rotation.h>
 #include "../src/workspace_checkers.h"
 #include "../src/workspace_samplers.h"
 
-using namespace boost::unit_test;
 using namespace rl::math;
 
-BOOST_AUTO_TEST_SUITE(test_workspace_checker_and_sampler)
+typedef boost::array<double, 3> a3;
 
-BOOST_AUTO_TEST_CASE(generate_and_check)
+TEST(WorkspaceCheckerSampler, checker_absolute_tolerance)
+{
+  WorkspaceChecker checker(BoxPositionChecker(Transform::Identity(), a3{0, 0, 0}, a3{0, 0, 0}),
+                           AroundTargetOrientationChecker(Rotation::Identity(), a3{0, 0, 0}, a3{0, 0, 0}));
+
+  auto transform = Transform::Identity();
+  transform.translate(Vector3(0, 0, 1e-8));
+
+  ASSERT_TRUE(checker.contains(transform));
+}
+
+TEST(WorkspaceCheckerSampler, generate_and_check)
 {
   const unsigned sample_attempts = 100;
 
@@ -26,13 +34,13 @@ BOOST_AUTO_TEST_CASE(generate_and_check)
   auto center_transform = Transform::Identity();
   center_transform.translate(translation).rotate(rotation);
 
-  typedef boost::array<double, 3> a;
-  a min_pos{-0.2, 0.1, 0};
-  a max_pos{-0.1, 0.4, 0};
-  a min_rot{-1, 0, 0.3};
-  a max_rot{-0.6, 0, 0.9};
+  a3 min_pos{-0.2, 0.1, 0};
+  a3 max_pos{-0.1, 0.4, 0};
+  a3 min_rot{-1, 0, 0.3};
+  a3 max_rot{-0.6, 0, 0.9};
 
-  BoxChecker checker(center_transform, min_pos, max_pos, min_rot, max_rot);
+  WorkspaceChecker checker(BoxPositionChecker(center_transform, min_pos, max_pos),
+                           AroundTargetOrientationChecker(center_transform.rotation(), min_rot, max_rot));
   WorkspaceSampler sampler(UniformPositionInAsymmetricBox(center_transform, min_pos, max_pos),
                            DeltaXYZOrientation(Quaternion(center_transform.rotation()), min_rot, max_rot));
 
@@ -45,8 +53,12 @@ BOOST_AUTO_TEST_CASE(generate_and_check)
   for (unsigned i = 0; i < sample_attempts; ++i)
   {
     auto sample = sampler.generate(sample_01);
-    BOOST_ASSERT(checker.contains(sample));
+    ASSERT_TRUE(checker.contains(sample));
   }
 }
 
-BOOST_AUTO_TEST_SUITE_END()
+int main(int argc, char** argv)
+{
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
