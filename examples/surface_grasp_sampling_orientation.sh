043cd610aa1c02034727284517706c7cd75cf90b
Ilia Krets <krets@campus.tu-berlin.de>
2019-02-01 11:10:10 +0100

    Change examples to work with current code

--- examples/surface_grasp_sampling_orientation.sh
+++ examples/surface_grasp_sampling_orientation.sh
@@ -11,29 +11,33 @@
 rosservice call /check_kinematics "
 initial_configuration: [0, 0.1, 0, 2.3, 0, 0.5, 0]
 goal_pose:
-  position: {x: 0.4, y: -0.02, z: 0.25}
+  position: {x: 0.4, y: -0.02, z: -0.096}
   orientation: {x: 0.997, y: 0, z: 0.071, w: 0}
 ifco_pose:
-  position: {x: 0.48, y: 0, z: 0.2}
+  position: {x: 0.48, y: 0, z: -0.146}
   orientation: {x: 0.0, y: 0.0, z: 0.7071081, w: 0.7071055}
+goal_manifold_frame:
+  position: {x: 0.4, y: -0.02, z: -0.096}
+  orientation: {x: 0.997, y: 0, z: 0.071, w: 0}
+goal_manifold_orientation: {x: 0.997, y: 0, z: 0.071, w: 0}
 bounding_boxes_with_poses:
 - box:
     type: 0
     dimensions: [0.08, 0.08, 0.08]
   pose:
-    position: {x: 0.4, y: -0.02, z: 0.25}
+    position: {x: 0.4, y: -0.02, z: -0.096}
     orientation: {x: 0.0, y: 0.0, z: 0.0, w: 1.0}
 - box:
     type: 0
     dimensions: [0.08, 0.08, 0.05]
   pose:
-    position: {x: 0.52, y: -0.01, z: 0.235}
+    position: {x: 0.52, y: -0.01, z: -0.111}
     orientation: {x: 0.0, y: 0.0, z: 0.0, w: 1.0}
 - box:
     type: 0
     dimensions: [0.04, 0.15, 0.04]
   pose:
-    position: {x: 0.6, y: 0.2, z: 0.24}
+    position: {x: 0.6, y: 0.2, z: -0.106}
     orientation: {x: 0, y: 0, z: 0.3826834, w: 0.9238795}
 min_position_deltas: [-0.05, -0.05, -0.07]
 max_position_deltas: [0.05, 0.05, 0.05]
