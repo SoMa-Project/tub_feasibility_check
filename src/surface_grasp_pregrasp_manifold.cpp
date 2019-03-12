42d6c8638b0b4c834162edcb640d7c3c45f055c1
Ilia Krets <krets@campus.tu-berlin.de>
2019-03-12 15:55:14 +0100

    Take axes directly from rotation matrix

--- src/surface_grasp_pregrasp_manifold.cpp
+++ src/surface_grasp_pregrasp_manifold.cpp
@@ -8,10 +8,13 @@ rl::math::Transform SurfaceGraspPregraspManifold::ManifoldSampler::generate(Samp
   double sampled_circle_x = sampled_radius * std::cos(sampled_phi);
   double sampled_circle_y = sampled_radius * std::sin(sampled_phi);
 
-  Eigen::Vector3d x_axis =
-      description_.initial_frame * Eigen::Vector3d::UnitX() - description_.initial_frame.translation();
-  Eigen::Vector3d y_axis =
-      description_.initial_frame * Eigen::Vector3d::UnitY() - description_.initial_frame.translation();
+  Eigen::Vector3d x_axis;
+  Eigen::Vector3d y_axis;
+  for (int i = 0; i < 3; ++i)
+  {
+    x_axis(i) = description_.initial_frame.linear()(i, 0);
+    y_axis(i) = description_.initial_frame.linear()(i, 1);
+  }
 
   Eigen::Vector3d sampled_point_on_circle =
       description_.initial_frame.translation() + x_axis * sampled_circle_x + y_axis * sampled_circle_y;
@@ -25,21 +28,24 @@ rl::math::Transform SurfaceGraspPregraspManifold::ManifoldSampler::generate(Samp
 
 bool SurfaceGraspPregraspManifold::ManifoldChecker::contains(const rl::math::Transform& transform_to_check) const
 {
-  Eigen::Vector3d x_axis =
-      description_.initial_frame * Eigen::Vector3d::UnitX() - description_.initial_frame.translation();
-  Eigen::Vector3d y_axis =
-      description_.initial_frame * Eigen::Vector3d::UnitY() - description_.initial_frame.translation();
-
   Eigen::Matrix<double, 3, 2> A;
   Eigen::Vector3d b;
+  Eigen::Vector3d x_axis;
+  Eigen::Vector3d y_axis;
 
   for (int i = 0; i < 3; ++i)
   {
+    x_axis(i) = description_.initial_frame.linear()(i, 0);
+    y_axis(i) = description_.initial_frame.linear()(i, 1);
+
+    // first column of A is X axis of frame
     A(i, 0) = x_axis(i);
+    // second column of A is Y axis of frame
     A(i, 1) = y_axis(i);
     b(i) = transform_to_check.translation()(i) - description_.initial_frame.translation()(i);
   }
 
+  // find the least squares solution for description_frame_origin + alpha * X_axis + beta * Y_axis = to_check_origin
   Eigen::Vector2d solution = A.jacobiSvd(Eigen::ComputeThinU | Eigen::ComputeThinV).solve(b);
   return transform_to_check.translation().isApprox(description_.initial_frame.translation() + solution(0) * x_axis +
                                                    solution(1) * y_axis);
